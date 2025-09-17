5ce5a9a8ce6c3714d55f8c365dc75f7fa8aaaf3b
Roopesh <Roopeshnani06@gmail.com>
2025-09-17 11:11:20 +0530

    Add RootFunction + root_handler to provide GET / (returns simple JSON) in SAM template

---
+++ lambda/root_handler.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+    body = {"status": "ok", "routes": ["/summarize (POST)"]}
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
+            "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+        },
+        "body": json.dumps(body)
+    }
